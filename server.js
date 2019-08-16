a819f357e506f03ff61a829fa540ab63b72029eb
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2019-08-16 17:54:33 -0300

    adding graphql

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String]
+        }
+
+        type RootMutation {
+            
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {}
+}));
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
